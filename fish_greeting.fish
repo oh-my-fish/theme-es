5b6de029e217c9349ad492c8693f83d97572ed40
eugenesvk <eugenesvk@gmail.com>
2016-01-25 20:27:44 +0300

    Added screenshots, removed greeting
    Also removed fish_greeting since it referenced custom functions that I decided shouldn't be part of the theme to avoid potential conflicts with user-defined functions

--- fish_greeting.fish
+++
@@ -1,4 +0,0 @@
-function fish_greeting
-  echo '`update` (homebrew and OMF), `fish_config`, `omf theme`'
-  echo '`z foo` cd to most recent dir matching `foo`; `z foo bar` matches both; `z -r foo` highest ranked; `z -t foo` most recent; `z -l foo` lists all dirs matching `foo` (by frequency)'
-end
